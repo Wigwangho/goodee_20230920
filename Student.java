7d07d6015a1cc355715741b3d3009adca4752b67
gwangho <145312516+Wigwangho@users.noreply.github.com>
2023-09-22 22:22:42 +0900

    Create Student.java
    Student 클래스 설정

---
+++ Student.java
@@ -0,0 +1,12 @@
+package com.week1;
+
+	//학생들의 성적을 기록하는 클래스
+public class Student {
+	//학생 클래스의 변수 선언
+	int 	html 		= 0;
+	int 	css 		= 0;
+	int 	java 		= 0;
+	String 	name 		= null;
+	int		sum 		= html+css+java;
+	double	avg			= sum/3;
+}
