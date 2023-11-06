7b06a195bd38af45066e54e65e22228182457ab9
gwangho <145312516+Wigwangho@users.noreply.github.com>
2023-11-06 23:54:50 +0900

    Create importmudule.js

---
+++ html/importmudule.js
@@ -0,0 +1,10 @@
+import * as all from './module.js';
+
+console.log(all);
+console.log(all.str);
+console.log(all.hap(2,3));
+const yourCar = new all.Sonata()
+console.log(yourCar.wheelNum);
+console.log(yourCar.speed);
+
+//module의 모든 요소들을 가져와 출력함
