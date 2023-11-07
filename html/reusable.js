ac2117d1807c79d56dc4a5907471e58c9e12512e
gwangho <145312516+Wigwangho@users.noreply.github.com>
2023-11-07 13:50:17 +0900

    Create reusable.js
    함수를 왜 써야 하는가?
    재사용을 용이하게 하여, 동일한 상황에 더 편하게 대처하기 위함이다

---
+++ html/reusable.js
@@ -0,0 +1,29 @@
+funcname = (firstname, lastname) =>{
+  return `${firstname} ${lastname}`;
+}
+
+
+
+let lastname = '나';
+let firstname ='신입';
+let fullname = `${lastname} ${firstname}`;
+
+console.log(fullname);
+console.log(funcname(firstname, lastname))
+console.log(funcname2(firstname, lastname))
+//arrow function을 사용하지 않았을 때의 function은 절차(순서)에 구애받지 않음
+
+function funcname2 (firstname, lastname){
+  return `${firstname} ${lastname}`;
+}
+
+lastname = '야';
+firstname ='나두';
+fullname = `${lastname} ${firstname}`;
+
+console.log(fullname);
+
+//일일히 let 붙여주고, 순서 바꾸고... 매우 귀찮다!
+//같은 작업이 반복될수록 더더욱 귀찮아진다
+//이럴 때 사용하는 것이 function
+
