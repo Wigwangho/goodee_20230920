9ac08babf768b23b2c02ebecb6bdf1415f8e1a21
gwangho <145312516+Wigwangho@users.noreply.github.com>
2023-11-09 17:35:17 +0900

    Update jsonstringify.js

--- html/jsonstringify.js
+++ html/jsonstringify.js
@@ -1,13 +1,15 @@
-const gildong = {
+const gildong = `{
   "name": "홍길동",
   "age": 25,
   "married": false,
   "family": { "father": "홍판서", "mother": "춘섬" },
   "hobbies": ["독서", "도술"],
   "jobs": null
-}
+}`
+
 const obj = JSON.parse(gildong);
 console.log(obj);
-
+console.log(obj.age);
+console.log(obj);
 const gildongLong = JSON.stringify(gildong, null, 2);
-console.log(str2);
+console.log(gildongLong);
